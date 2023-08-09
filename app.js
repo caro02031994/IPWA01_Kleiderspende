b2e90eafd7bd2abae28bfe9c9eacc80dbe514c9b
caro02031994 <127253542+caro02031994@users.noreply.github.com>
2023-08-09 12:28:06 +0200

    09.08

--- app.js
+++ app.js
@@ -9,28 +9,63 @@ new Vue({
     plz: '',
     ort: '',
     abholung: '',
+    message: '',
+    
+    baby: false,
+    kkind: false,
+    kind: false,
+    damen: false,
+    herren: false,
+    kschuhe: false,
+    dschuhe: false,
+    hschuhe: false,
+    textil: false,
+
+    showMessage: false,
+    
     showAddress: true,
-       },
-    methods: {
 
-        submitForm() {
-            const timestamp = new Date().toLocaleString();
-            // Meldung setzen
-            this.message = `Formular abgesendet am ${timestamp}`;
-          },
+    submitted: false,
+  
 
-    toggleAddress() {
-    if (this.abholung === 'Abholung') {
-    this.showAddress = true;
-    } else {
-    this.showAddress = false;
-    }
     },
-    checkPLZ() {
-    if (this.plz.length >= 2 && this.plz.substring(0, 2) !== '37') {
-    alert('Leider wohnen Sie außerhalb des Abholgebietes. Bitte geben Sie Ihre Spende in der Geschäftstelle ab.');
-    this.plz = '';
-    }
-    }
+
+      
+    
+    methods: {
+    
+        submitForm() {
+            this.submitted = true;
+
+             // Datum und Uhrzeit
+            const currentDate = new Date();
+            const currentTime = currentDate.toLocaleTimeString(); 
+
+      // Meldung für Zeitausgabe
+            this.message = ` ${currentDate.toLocaleDateString()} um  ${currentTime}`;
+
+      
+            this.showMessage = true;
+          },
+      
+
+         toggleAddress() {
+            if (this.abholung === 'abgeholt') {
+            this.showAddress = true;
+                } else {
+                this.showAddress = false;
+                }
+        },
+
+
+    
+        checkPLZ() {
+            if (this.plz.length >= 2 && this.plz.substring(0, 2) !== '37') {
+            alert('Leider wohnen Sie außerhalb des Abholgebietes. Bitte geben Sie Ihre Spende in der Geschäftstelle ab.');
+            this.plz = '';
+            }
+        }
 }
+
+
 });
